aa09230466dc9a27da36f32d257752f2a7262477
DinaShantina <dinadamjanovik@gmail.com>
2022-12-06 23:17:29 +0100

    added underconstruction home page

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import WelcomePage from './WelcomePage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <WelcomePage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
